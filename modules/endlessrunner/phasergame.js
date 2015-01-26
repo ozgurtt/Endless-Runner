9926b89a6fa90b6b50b13313ea443c7179cfdbc8
Dariusz Parys <dariusz@kouder.net>
2015-01-26 10:55:06 +0100

    Update phasergame.js

--- modules/endlessrunner/phasergame.js
+++ modules/endlessrunner/phasergame.js
@@ -3,7 +3,7 @@
 ], function (Phaser) {
 
     var PhaserGame = function (w, h) {
-        return new Phaser.Game(w, h, Phaser.EXACT_FIT);
+        return new Phaser.Game(w, h, Phaser.AUTO);
     };
 
     return PhaserGame;
